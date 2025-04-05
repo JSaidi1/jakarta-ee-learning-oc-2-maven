bd4ac790947c333ae24bf2985ffbf56a4682da7c
JSaidi1 <jabrane_saidi@outlook.com>
2025-04-05 17:38:37 +0200

    Multi-tiers Maven Project [https://openclassrooms.com/fr/courses/4503526-organisez-et-packagez-une-application-java-avec-apache-maven]

---
+++ ticket-webapp/src/main/java/MaServlet.java
@@ -0,0 +1,31 @@
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class MaServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "";
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
